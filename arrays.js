bb217c9d342042b75757e2eab14a6164c36c851b
alicesantanna-create <alice.santanna@escola.pr.gov.br>
2026-04-28 15:28:25 +0000

    trabalho 1 finalizado

---
+++ arrays.js
@@ -0,0 +1,51 @@
+const edFolha = [
+  {
+  titulo: "O Pequeno Príncipe ",
+  preco: 14
+  },
+  {
+    titulo: "Prada ",
+    preco: 66
+  },
+  {
+    titulo: "Chanel",
+    preco: 67
+  },
+  {
+    titulo: "Hermès",
+    preco: 72
+  },
+  {
+    titulo: "Christian Louboutin",
+    preco: 68
+  }
+]
+
+const edGalho = [
+  {
+    titulo: "Louis Vuitton",
+    preco: 68
+  },
+  {
+    titulo: "Little Book of Valentino",
+    preco: 91
+  },
+  {
+    titulo: "Little Book of Dior",
+    preco: 77
+  },
+  {
+    titulo: "Little Book of Gucci",
+    preco: 74
+  },
+  {
+    titulo: "Little Book of Alexander McQueen",
+    preco: 67
+  },
+  {
+    titulo: "Little Book of Balenciaga",
+    preco: 73
+  }
+]
+
+module.exports = { edFolha, edGalho }
